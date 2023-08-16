c4bf40e069997d4ac68455be538f1657cee3253a
William dos Santos <willdossantosdesigns@gmail.com>
2023-08-15 22:00:45 -0700

    work history page

--- pages/work-history.js
+++ pages/work-history.js
@@ -11,17 +11,29 @@ const WorkHistory = () => {
     <div className="container">
       <Sidebar />
       <main>
-        <Navbar/>
+        <Navbar />
         <HeaderQuote />
-        <h1>Work History</h1>
-
-        {/* Add your content here */}
-        <p>
-          This is a brief description of my work history. I will add more
-          details later.
-        </p>
-
-        {/* Link back to the home page or any other page */}
+        <div className="inner-page-container" style={{ position: 'relative' }}>
+          <div className="timeline">
+            <div className="work-card work-card--left">
+                <div className="work-card-date">Current</div>
+                <div className="work-card--logo"></div>
+                <div className="work-card-main">
+                    <h3>Career Update: Executive Creative Director</h3>
+                    <p>Are you looking for a new job opportunity? Consider making the switch from your current company to a new one! With a new job, you can gain new experiences, learn new skills, and take your career to the next level.</p>
+                </div>
+            </div>
+            <div className="work-card work-card--right">
+                <div className="work-card-date">Current</div>
+                <div className="work-card--logo"></div>
+                <div className="work-card-main">
+                    <h3>Career Update: Executive Creative Director</h3>
+                    <p>Are you looking for a new job opportunity? Consider making the switch from your current company to a new one! With a new job, you can gain new experiences, learn new skills, and take your career to the next level.</p>
+                </div>
+            </div>
+          </div>
+          {/* Link back to the home page or any other page */}
+        </div>
       </main>
     </div>
   );
