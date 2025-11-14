027d3f35cb0708d6eaf0913b21f25ad9cd7e5670
William dos Santos <willdossantosdesigns@gmail.com>
2025-11-14 11:33:40 -0700

    updates

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,17 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link 
+          rel="preload" 
+          href="https://fonts.googleapis.com/css2?family=Lazare+Grotesk:wght@300;400;600;700&display=swap" 
+          as="style" 
+        />
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@1,900&family=Lazare+Grotesk:wght@300;400;600;700&family=Poppins:wght@300;400;600;700&display=swap" 
+          rel="stylesheet" 
+        />
       </Head>
       <body>
         <Main />
